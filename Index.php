1b604ec267c6347182905e81ca94c5c31c14a249
Youma <youmadrame301@gmail.com>
2025-05-27 15:27:54 +0200

    first commit

---
+++ Index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Fleur'shop</title>
+    <link rel="stylesheet" href="styles.css">
+    <link  href="https://fonts.googleapis.com/css2?family=Great+Vibes&family=Open+Sans&display=swap" rel="stylesheet">
+</head>
+<body>
+ <?php
+ include 'header.php';
+ ?>
+  <main>
+    <section class="hero">
+         <img src="rose.png" alt="Fleurs décoratives" class="fleur-img">
+        <div class="text">
+            <p class="slogan">Envoyez des fleurs, Offrez du bonheur</p>
+            <p class="description">Chez fleur'shop, nous vous offrons un service floral de qualité pour toutes les occasions...</p>
+            <a href="#" class="bouton">Découvrir nos bouquets</a>
+        </div>
+    </section>
+  </main>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,12 @@
+<header>
+    <h1 class="logo">Fleur'shop</h1>
+    <nav>
+        <ul>
+            <li><a href="#">Accueil</a></li>
+            <li><a href="#">Catalogue</a></li>
+            <li><a href="#">Produit</a></li>
+            <li><a href="#">Panier</a></li>
+            <li><a href="#">Connexion</a></li>
+        </ul>
+    </nav>
+  </header>  
